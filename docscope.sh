458d773d76d0c5a5339f81b669fa8ad4e5814882
andy.xs <andy.xs@alibaba-inc.com>
2016-07-20 09:18:48 +0800

    up cscope script
    Change-Id: I223d6b8b91b1ef13b8e432f396dbafc9a05a02e0

--- docscope.sh
+++ docscope.sh
@@ -3,7 +3,7 @@
 echo "begin create cscope database"
 if [ -e "$MYWORKSPACE" ]; then
     rm -f $MYWORKSPACE/cscope*
-    find $MYWORKSPACE -type f -name '*.h' -o -name '*.hpp' -o -name '*.c' -o -name '*.cpp' > $MYWORKSPACE/cscope.files
+    find $MYWORKSPACE -type f | egrep -v ".*\.a.*" | egrep -v ".*\.so.*" > $MYWORKSPACE/cscope.files
     cd $MYWORKSPACE
     cscope -bq
 fi
